d1ce706c61a9c80cc5cef70910b4cd9bc9eaaba8
Cameron Dimitroff <hawtrods@gmail.com>
2018-04-28 20:16:46 -0700

    yeup

---
+++ app/data/friends.js
@@ -0,0 +1,8 @@
+const friends = [
+  {
+    name: "Aziz",
+    photo:
+      "https://ia.media-imdb.com/images/M/MV5BMjE5NzYwNDQwN15BMl5BanBnXkFtZTgwNDA2NjQwODE@._V1_UX214_CR0,0,214,317_AL_.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  }
+];
